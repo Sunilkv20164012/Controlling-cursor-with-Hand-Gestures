02db4b4953163bafee742d66ad330c1f31931adb
Sunil Kumar <sunilkv20164012@gmail.com>
2019-01-28 20:19:19 +0100

    frontend and capturing image from webcam

---
+++ JavaServer/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root, 900, 690);
+
+        primaryStage.setTitle("WebCam Capture Sarxos API using JavaFx with FXML ");
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
